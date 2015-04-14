46076d80366e7dc99ee647ba984680081d14838f
José Salinas <hola@josesalinas.com>
2015-04-15 00:35:38 +0200

    Improved the followed_by? check in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,15 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  has_many :messages
+  has_many :events
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
   validates :name, presence: true, uniqueness: true
   
-  has_many :messages
-  
-  has_many :events
-
   def followed_by?(user_id)
-    !Event.where(user_id: user_id, user_target_id: self.id).empty?
+    Event.exists?(user_id: user_id, user_target_id: self.id)
   end
   
 end
