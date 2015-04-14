615867417cb39fa729676a815597b73ffd62e67c
José Salinas <hola@josesalinas.com>
2015-04-14 13:53:08 +0200

    Initial commit, with latest ruby and rails versions. Haml included. Also resources folder is present with the logo and the code challenge instructions

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hereby::Application.config.secret_key_base = '36ddccaf5905959d9edca8c906bcb5164682c8241ee120302bc741521e7f425a7cda37d83a35a9a62e055894e8c99ff5ce52f62fce84cdb3a818f0d08d7dbce0'
